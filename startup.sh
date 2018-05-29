1d45b7be5ef86046957f0de0b1fecc72bdbee081
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-05-29 21:04:10 +0900

    Refactoring

--- startup.sh
+++ startup.sh
@@ -8,6 +8,5 @@ fi
 sudo Xvfb :0 -screen 0 "${size}x24" -nolisten tcp &
 sleep 2
 sudo x11vnc -display :0 -loop -reopen -shared -localhost -rfbport 5900 -rfbportv6 -1 -repeat -nopw &
-export DISPLAY=:0
 sudo xrdp
-startxfce4
+DISPLAY=:0 startxfce4
