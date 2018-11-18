a6f1fa614ca3cec8e85ef95c83d59e12a717b9cb
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-11-18 19:09:22 +0900

    WIP: Subtask of wm remains even if wm is terminated in VNC mode

--- startup.sh
+++ startup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-set -euo pipefail
-
-function cleanup (){
-    set +e
-    sudo kill $(sudo ps -e | grep xrdp | awk '{print $1}')
-    sudo kill $(sudo ps -e | grep x11vnc | awk '{print $1}')
-    sudo kill $(sudo ps -e | grep Xvfb | awk '{print $1}')
-    sleep 1
-    sudo rm -f "/tmp/.X11-unix/X${display}"
-    exit 0
-}
-trap cleanup 1 2 3 15
+set -uo pipefail
 
 if [[ $# -ge 1 ]] ;then
     size="${1}x24"
@@ -24,16 +13,26 @@ if [[ $# -ge 2 ]] ;then
     else
         display=10
     fi
-    sudo Xvfb ":${display}" -screen 0 "${size}" -nolisten tcp &
-    sudo x11vnc -display ":${display}" -loop -reopen -shared -localhost -rfbport "${vncport}" -rfbportv6 -1 -repeat -nopw &
+
+    sudo rm -f "/tmp/.X11-unix/X${display}"
+    cat <<EOF | sudo tee /etc/supervisor/conf.d/Xvfb_x11vnc.conf > /dev/null
+[program:Xvfb]
+command=/usr/bin/Xvfb ":${display}" -screen 0 "${size}" -nolisten tcp
+priority=1
+[program:x11vnc]
+command=/usr/bin/x11vnc -display ":${display}" -reopen -shared -rfbport "${vncport}" -rfbportv6 -1 -repeat -nopw
+priority=2
+EOF
+
+    export DISPLAY=":${display}"
+    sudo supervisord -c /etc/supervisor/supervisord.conf
     sleep 2
-    DISPLAY=":${display}" startxfce4 &
-    child=$!
-    wait ${child}
+    while :
+    do
+        startxfce4
+    done
 else
+    sudo rm -f "/tmp/.X11-unix/X10"
     sudo xrdp
-    set +e
-    sudo xrdp-sesman -n
-    set -e
+    exec sudo xrdp-sesman -n
 fi
-cleanup
