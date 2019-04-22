709b73e21bbf97d8c3259b48b51230cf2f8f4f9f
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-04-22 12:09:33 +0900

    Add workaround for browser

--- xvfb.sh
+++ xvfb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -euo pipefail
-docker run --rm -d -v /tmp/.X11-unix:/tmp/.X11-unix -v "${HOME}:/home/${LOGNAME}" --name xvfb kiyoad/xvfb "${LOGNAME}" "${LOGNAME}" "$(id -u)" "$@"
+docker run --rm -d -v /dev/shm:/dev/shm -v /tmp/.X11-unix:/tmp/.X11-unix -v "${HOME}:/home/${LOGNAME}" --name xvfb kiyoad/xvfb "${LOGNAME}" "${LOGNAME}" "$(id -u)" "$@"
