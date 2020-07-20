60548893d320d9d417a76a796eae6268100f7063
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2020-07-20 19:25:55 +0900

    Use docker-init in xvfb container

--- xvfb.sh
+++ xvfb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 PASSWORD="${LOGNAME}"
-docker run --rm -d -v /dev/shm:/dev/shm -v /tmp/.X11-unix:/tmp/.X11-unix -v "xvfb_${LOGNAME}:/home/xvfb" -v "${HOME}:/home/${LOGNAME}" --hostname xvfb --name xvfb kiyoad/xvfb "${PASSWORD}" "$(id -u)" "$@"
+docker run --init --rm -d -v /dev/shm:/dev/shm -v /tmp/.X11-unix:/tmp/.X11-unix -v "xvfb_${LOGNAME}:/home/xvfb" -v "${HOME}:/home/${LOGNAME}" --hostname xvfb --name xvfb kiyoad/xvfb "${PASSWORD}" "$(id -u)" "$@"
